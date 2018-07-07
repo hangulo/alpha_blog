fea1e81ef8e6543c7b61dc0819dbbda3d78d1688
Hector Angulo <mail@angulohome.com>
2018-07-07 06:23:46 +0000

    Add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,15 @@
 class ArticlesController < ApplicationController
+
+    def index
+        @articles = Article.all
+    end
+
     def new
         @article = Article.new
     end
     
     def create
+        #render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was succesfully created"
@@ -18,6 +24,22 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         
     end
+    
+    def edit
+       @article = Article.find(params[:id]) 
+    end
+    
+    def update
+        @article = Article.find(params[:id]) 
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            
+        end
+            
+    end
+    
     
     private
         def article_params
